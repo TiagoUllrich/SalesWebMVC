6977903a2318a644bde0175a2978461948d659fa
Tiago Ullrich <tiagofu@gmail.com>
2020-06-26 07:11:33 -0300

    SellerService and basic FindAll

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() //Encontrar através do objeto _context uma lista de todos os vendedores. Esse método é sincrono, ou seja, toda a aplicação irá travar enquanto roda
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
